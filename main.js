bb620a6fc39d8bb54d4bb5fea3fc7789385f0d6a
Vistonho <86853661+Vistonho@users.noreply.github.com>
2021-10-08 13:56:20 +0300

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const image =  document.querySelector('https://www.belanta.vet/vet-blog/wp-content/uploads/2019/09/1-6.jpg');
-    image.classList.add('animated', 'pulse');
-});
