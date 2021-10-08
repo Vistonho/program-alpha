cf409b607c9b6e2b4c2884f7af9bc3e0e13bf4db
Vistonho <86853661+Vistonho@users.noreply.github.com>
2021-10-08 13:57:06 +0300

    Add files via upload

---
+++ UO3_1.php
@@ -0,0 +1,16 @@
+<?php
+  $a = "blue";
+
+  if ($a == "red"){
+    echo "Красный";
+  }
+  elseif ($a == "green"){
+    echo "зеленый";
+  }
+  elseif ($a == "blue"){
+    echo "голубой";
+    }
+  else{
+    echo ('Цвет не определен');
+  }
+ ?>
